6f8fd1a21760eda299648dc02cbe9179a8712fcf
Avnit Bambah <avnitbambah@Avnits-iMac.lan>
2023-08-12 22:42:12 -0700

    Changing gcs fuse file location

--- app/gcsfuse_run.sh
+++ app/gcsfuse_run.sh
@@ -5,5 +5,6 @@ set -eo pipefail
 mkdir -p $MNT_DIR
 
 echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
+# gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
+gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
 echo "Mounting completed."
