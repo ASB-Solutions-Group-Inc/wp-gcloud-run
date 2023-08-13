95339537b7f1b6eeac6174700e8bfa9a5b2f3728
Avnit bambah <avnitbambah@asbsolutionsgroup.com>
2023-08-13 03:59:20 -0700

    Changing gcs fuse file location

--- app/gcsfuse_run.sh
+++ app/gcsfuse_run.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # Create mount directory for service
 mkdir -p $MNT_DIR
 
-export DISK_BUCKET=gs://stateless-wordpress-gcloud-run-wp-demo
+export DISK_BUCKET=stateless-wordpress-gcloud-run-wp-demo
 
 echo "Mounting GCS Fuse."
 # gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
