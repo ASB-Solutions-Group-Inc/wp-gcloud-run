a994276a9bd94926cc49d39da38a541a9a1fbf83
Avnit bambah <avnitbambah@asbsolutionsgroup.com>
2023-08-13 04:56:48 -0700

    Changing gcs fuse file location

--- app/gcsfuse_run.sh
+++ app/gcsfuse_run.sh
@@ -7,6 +7,6 @@ mkdir -p $MNT_DIR
 export DISK_BUCKET=stateless-wordpress-gcloud-run-wp-demo
 
 echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
+gcsfuse --debug_gcs --debug_fuse $DISK_BUCKET $MNT_DIR
 # gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
 echo "Mounting completed."
