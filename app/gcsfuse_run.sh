1169ddc6e552e3bd88d8d601d6314890964f47c6
Avnit bambah <avnitbambah@asbsolutionsgroup.com>
2023-08-20 16:45:42 -0700

    Removing foreground

--- app/gcsfuse_run.sh
+++ app/gcsfuse_run.sh
@@ -14,6 +14,6 @@ echo "Mounting GCS Fuse."
 #gcsfuse --debug_gcs --debug_fuse $DISK_BUCKET $MNT_DIR
 #gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --foreground  --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
 #gcsfuse --key-file /var/www/html/service_account_conf.json -o allow_other --file-mode 777 --dir-mode 777 --foreground --implicit-dirs $DISK_BUCKET $MNT_DIR
-gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
+gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
 echo "Mounting completed."
 apache2ctl -D FOREGROUND
