d68c28ef8132d6967ce2f7ca1ddd740054bafb86
Avnit bambah <avnitbambah@asbsolutionsgroup.com>
2023-08-20 16:04:15 -0700

    changing bicket location

--- app/gcsfuse_run.sh
+++ app/gcsfuse_run.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # Create mount directory for service
 mkdir -p $MNT_DIR
 
-export DISK_BUCKET=stateless-wordpress-gcloud-run-wp-demo
+export DISK_BUCKET=209158737525-wp-cloudrun-demo
 # export FIRESTORE_IP_ADDRESS=
 
 echo "Mounting GCS Fuse."
