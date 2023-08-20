7eb8b9f3bae70b6c17b17e7e447eb0fd73a51f8c
Avnit bambah <avnitbambah@asbsolutionsgroup.com>
2023-08-20 15:19:20 -0700

    removing extra items from the gcs fuse

--- app/gcsfuse_run.sh
+++ app/gcsfuse_run.sh
@@ -10,7 +10,8 @@ export DISK_BUCKET=stateless-wordpress-gcloud-run-wp-demo
 echo "Mounting GCS Fuse."
 # mount -o nolock $FILESTORE_IP_ADDRESS:/$FILE_SHARE_NAME $MNT_DIR
 #gcsfuse --debug_gcs --debug_fuse $DISK_BUCKET $MNT_DIR
+gcsfuse -o allow_other --foreground  --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
 #gcsfuse --key-file /var/www/html/service_account_conf.json -o allow_other --file-mode 777 --dir-mode 777 --foreground --implicit-dirs $DISK_BUCKET $MNT_DIR
-gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
+# gcsfuse -o rw,allow_other -file-mode=777 -dir-mode=777 --foreground --debug_http --debug_gcs --debug_fuse --implicit-dirs $DISK_BUCKET $MNT_DIR
 echo "Mounting completed."
 apache2ctl -D FOREGROUND
